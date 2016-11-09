b0b69ff8e64773c2d65f80687445bdc6508256ba
Gesantung <gesan@thecover.cn>
2016-11-09 16:06:31 +0800

    vr demo

--- GTVRPlayer/GTVRPlayer/GameViewController.swift
+++ GTVRPlayer/GTVRPlayer/GameViewController.swift
@@ -9,33 +9,66 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import CoreMotion
+import SpriteKit
+import AVFoundation
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController,SCNSceneRendererDelegate {
+    
+    let cameraNode = SCNNode()
+    
+    var sceneView = SCNView()
+    
+    //球形天空盒
+    func createSphereNode(material: AnyObject?) -> SCNNode {
+        let sphere = SCNSphere(radius: 40.0)
+        sphere.firstMaterial!.isDoubleSided = true
+        sphere.firstMaterial!.diffuse.contents = material
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3Make(0,0,0)
+        return sphereNode
+    }
+    
+    func configureScene(node sphereNode: SCNNode) {
+        // Set the scene
+        sceneView.frame = self.view.frame
+        self.view.addSubview(sceneView)
+        let scene = SCNScene()
+        sceneView.scene = scene
+        //sceneView.showsStatistics = true
+        sceneView.allowsCameraControl = true
+        // Camera, ...
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3Make(0, 0, 0)
+        scene.rootNode.addChildNode(sphereNode)
+        scene.rootNode.addChildNode(cameraNode)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let videoName = "skyrim360"
+        
+        let fileURL: NSURL? = NSURL.fileURL(withPath: Bundle.main.path(forResource: videoName, ofType: "mp4")!) as NSURL?
+        
+        let player = AVPlayer(url: fileURL as! URL)
+        let videoNode = SKVideoNode(avPlayer: player)
+        let size = CGSize(width: 1024,height: 1024)
+        videoNode.size = size
+        videoNode.position = CGPoint(x: size.width/2.0,y: size.height/2.0)
+        let spriteScene = SKScene(size: size)
+        spriteScene.addChild(videoNode)
+        let sphereNode = createSphereNode(material:spriteScene)
+        configureScene(node: sphereNode)
     }
     
-    override var shouldAutorotate: Bool {
-        return true
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+    override func viewDidAppear(_ animated: Bool) {
+        sceneView.play(self)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+        // Dispose of any resources that can be recreated.
     }
 
 }
